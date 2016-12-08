addaa0457efc966661465ec46e13c03944960348
Jean Carlo Nascimento <jnascimento@gmail.com>
2016-12-08 17:14:01 -0200

    Update isName.js

--- isName/isName.js
+++ isName/isName.js
@@ -1,13 +1,10 @@
-'use strict';
-
 module.exports = (value) => {
 
-	const regex = /^([ \u00c0-\u01ffa-zA-Z'\-])+$/g;
-	const isEmpty = require('../isEmpty/isEmpty')(value);
-	if(isEmpty) return false;
-
-	const isString = require('../isString/isString')(value);
-	if(!isString) return false;
+	const regex = /^([ \u00c0-\u01ffa-zA-Z'\-])+$/g
+	const isEmpty = require('../isEmpty/isEmpty')(value)
+	const isString = require('../isString/isString')(value)
+	
+	if (isEmpty || !isString) return false
 
 	return regex.test(value);
 
